9043c942869445ecf800b4eadd374dcd00ee0728
sbx_user1051 <sbx_user1051@169.254.132.109>
2024-07-07 21:11:25 +0000

    Sun Jul 07 21:11:25 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -18,12 +18,12 @@ def checkout(skus):
 
     for k, v in basket.items():
         if k == 'A':
-            if v >= 3:
+            if v % 3 != 0:
                 result += ((v // 3) * 130) + ((v % 3) * 50)
             else:
                 result += v * 50
         elif k == 'B':
-            if v >= 2:
+            if v % 2 != 0:
                 result += ((v // 2) * 45) + ((v % 2) * 30)
             else:
                 result += v * 30
@@ -32,10 +32,11 @@ def checkout(skus):
         elif k == 'D':
             result += v * 15
         elif k == 'E':
-            if v >= 2:
+            if v % 2 == 0:
                 result += ((v // 2) * 30) + (v * 40)
             else:
                 result += v * 40
 
 
     return result
+
