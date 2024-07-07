577eee6e663a56b00b8ed7d07e48a0cdef8172ac
sbx_user1051 <sbx_user1051@169.254.132.109>
2024-07-07 22:08:58 +0000

    Sun Jul 07 22:08:58 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,7 +5,10 @@
 def checkout(skus):
     items = {
         'A': 50,
+        '3A': 130,
+        '5A': 200,
         'B': 30,
+        '2B': 45,
         'C': 20,
         'D': 15,
         'E': 40,
@@ -26,23 +29,26 @@ def checkout(skus):
     if 'E' in basket:
         if basket['E'] >= 2:
             if 'B' in basket:
-                print(basket)
                 basket['B'] -= (basket['E'] // 2)
-                print(basket)
+
+    if 'F' in basket:
+        if basket['F'] >= 2:
+            basket['F'] -= (basket['F'] // 2)
+
 
     for k, v in basket.items():
         if k == 'A':
             if v >= 3:
-                result += (v // 5) * 200
+                result += (v // 5) * items['5A']
                 rem = v % 5
-                result += (rem // 3) * 130
+                result += (rem // 3) * items['3A']
                 rem1 = rem % 3
                 result += rem1 * items['A']
             else:
                 result += v * items['A']
         elif k == 'B':
             if v >= 2:
-                result += ((v // 2) * 45) + ((v % 2) * items['B'])
+                result += ((v // 2) * items['2B']) + ((v % 2) * items['B'])
             else:
                 result += v * items['B']
         elif k == 'C':
@@ -55,6 +61,7 @@ def checkout(skus):
             result += v * items['F']
 
     return result
+
 
 
 
